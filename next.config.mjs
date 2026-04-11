aa9d0c92e830809e4ee0f8d6dc4833bc44faf820
fikri <fikrialfarizi039@gmail.com>
2026-04-11 22:07:46 +0700

    p

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
+  transpilePackages: ['bot'],
   images: {
     remotePatterns: [
       {
