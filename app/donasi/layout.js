72b3885f92ce5479883f998de310c627961bf708
fikri <fikrialfarizi039@gmail.com>
2026-04-21 23:26:00 +0700

    Auto push from Bill

--- app/donasi/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Donasi Santuy",
-  description: "Dukung Teknologi Santuy agar tetap eksis dan terus memberikan update game serta konten teknologi terbaik untuk kalian.",
-};
-
-export default function DonasiLayout({ children }) {
-  return <>{children}</>;
-}
